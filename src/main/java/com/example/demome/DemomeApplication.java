856068176a5e05b8a0174e60407eb6f0c482edee
gaoyong234 <326363139@qq.com>
2020-11-27 10:16:08 +0800

    Initial commit

---
+++ src/main/java/com/example/demome/DemomeApplication.java
@@ -0,0 +1,15 @@
+package com.example.demome;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.example.demome.dao")
+public class DemomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemomeApplication.class, args);
+    }
+
+}
